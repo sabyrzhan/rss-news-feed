31944dca929f6cecba0ef12cdfc540c7c98c7bee
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2022-11-07 19:47:08 +0200

    Add readRequestData method to read data from ServerSocket. But reading from InputStream hangs indefinitely.

--- src/main/java/kz/sabyrzhan/rssnewsfeed/Service.java
+++ src/main/java/kz/sabyrzhan/rssnewsfeed/Service.java
@@ -1,12 +1,15 @@
 package kz.sabyrzhan.rssnewsfeed;
 
+import com.sun.net.httpserver.HttpServer;
 import org.apache.commons.io.IOUtils;
 import rawhttp.core.RawHttp;
 
 import java.io.*;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.SocketTimeoutException;
 import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 import java.util.concurrent.Executors;
 
 public class Service {
@@ -18,25 +21,39 @@ public class Service {
 
         while(true) {
             try {
-                System.out.println("Waiting for client on port " +
-                        serverSocket.getLocalPort() + "...");
                 var server = serverSocket.accept();
-                String requestData = IOUtils.toString(server.getInputStream(), StandardCharsets.UTF_8);
+
                 executors.submit(() -> {
                     try {
-                        System.out.println("Just connected to " + server.getRemoteSocketAddress());
-                        var rawHttp = new RawHttp();
-
-
-                        var request = rawHttp.parseRequest(requestData);
-                        request.writeTo(server.getOutputStream());
-
+                        System.out.println("Waiting for client on port " +
+                                serverSocket.getLocalPort() + "...");
+                        String data = readRequestData(server.getInputStream());
+                        System.out.println("Data is: " + data);
+                        //var data = IOUtils.toString(new InputStreamReader(server.getInputStream()));
+                        System.out.println("Data: " + data);
+                        var response = "HTTP/1.1 200 OK\r\n" +
+                                "Content-Type: text/plain\r\n" +
+                                "Content-Length: 9\r\n" +
+                                "\r\n" +
+                                "something";
+                        var out = new BufferedWriter(
+                                new OutputStreamWriter(
+                                        new BufferedOutputStream(server.getOutputStream()), "UTF-8")
+                        );
+                        out.write(response);
+                        out.flush();
+                        out.close();
+                        server.close();
+                        System.out.println("Closed");
                     } catch (Exception e) {
+                        System.out.println("Executor error:" + e.toString());
                         e.printStackTrace();
                     } finally {
                         try {
+                            System.out.println("Closing server");
                             server.close();
                         } catch (Exception e) {
+                            System.out.println("Executor error2: ");
                             e.printStackTrace();
                         }
                     }
@@ -45,9 +62,42 @@ public class Service {
                 System.out.println("Socket timed out!");
                 break;
             } catch (IOException e) {
+                System.out.println("Error");
                 e.printStackTrace();
                 break;
             }
         }
+
+        serverSocket.close();
+    }
+
+    private String readRequestData(InputStream socketInputStream) {
+        String result = "";
+        try {
+            var br = new BufferedReader(new InputStreamReader(socketInputStream));
+            String data = null;
+            int line = 1;
+            int emptyCounter = 0;
+            while ((data = br.readLine()) != null) {
+                System.out.println((line++) + ") " + data);
+                result += data;
+                // this seems to be the key for me!
+                // somehow I never get out of this loop if I don't
+                // check for an empty line...
+                if (data.isEmpty()) {
+                    if (emptyCounter >= 1) {
+                        System.out.println("Emptycounter: " + emptyCounter + ". Finishing");
+                        break;
+                    } else {
+                        emptyCounter++;
+                        System.out.println("Empty: " + emptyCounter);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 }
